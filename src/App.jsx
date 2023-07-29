936e4e60a5966ce599ee68ba49cd6d7383cd4c59
konfle <konrad.flejter@gmail.com>
2023-07-29 16:12:59 +0200

    Add routing to register and login button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    },
+  ]);
+
   return (
     <div>
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 }
